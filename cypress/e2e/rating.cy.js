c82ab85b5b4af62909d40c74378bc89c0f5e346d
Kristi Ingco <kristiana.d.ingco@gmail.com>
2024-10-10 20:22:26 -0700

    Add test for one star rating

--- cypress/e2e/rating.cy.js
+++ cypress/e2e/rating.cy.js
@@ -1,8 +1,11 @@
 /// <reference types = "cypress"/>
 
 describe("Rating Functionality", () => {
-    it("should load the page", () => {
+    beforeEach(() => {
         cy.visit("/");
+    });
+
+    it("should load the page", () => {
         cy.get(".text").then(($element) => {
             const computedStyle = window.getComputedStyle(
                 $element[0],
@@ -12,6 +15,32 @@ describe("Rating Functionality", () => {
             const content = computedStyle.getPropertyValue("content");
 
             expect(content).to.contain("Rate your experience");
+        });
+    });
+
+    it("should rate one star", () => {
+        cy.get(".star-1").click();
+
+        cy.get(".text").then(($element) => {
+            const computedStyle = window.getComputedStyle(
+                $element[0],
+                "::before"
+            );
+
+            const content = computedStyle.getPropertyValue("content");
+
+            expect(content).to.contain("I just hate it");
+        });
+
+        cy.get(".numb").then(($element) => {
+            const computedStyle = window.getComputedStyle(
+                $element[0],
+                "::before"
+            );
+
+            const content = computedStyle.getPropertyValue("content");
+
+            expect(content).to.contain("1 out of 5");
         });
     });
 });
